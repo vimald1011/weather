6db0b26104b242b16eedf770d9b7f608b29e2c30
vimal dubey <vimald1011@gmail.com>
2024-03-03 06:47:04 +0530

    now improve static files

--- index.js
+++ index.js
@@ -10,16 +10,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 
-app.get("/", (req, res) => {
-    res.render("index", {
-        krishna: "",
-        tempt: 23,
-        location: "India",
-        windSpeed: "5",
-        humidity: 1.2
-    }); // Render the index without passing any additional data
-});
-
 app.get("/getData", (req, res) => {
     const city = req.query.citty;
     const url = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${token}`;
@@ -28,13 +18,12 @@ app.get("/getData", (req, res) => {
         const response = await fetch(url);
         const data = await response.json();
         res.render("index", {
-            krishna: radha,
             tempt: Math.round(data.main.temp),
             location: data.name,
             windSpeed: Math.round(data.wind.speed),
             humidity: Math.round(data.main.humidity)
 
-        }); // Render the index with krishna value
+        });
     }
 });
 
