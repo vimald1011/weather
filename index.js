8f0ad1abfde26fbe711c7c84d5890b73be5864ce
vimal dubey <vimald1011@gmail.com>
2024-03-12 04:03:42 +0530

    removed unwanted comments

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 
+app.get("/vi", (req, res) => {
+    res.render("public/index.html", {
+        tempt: 23,
+        location: "India",
+        windSpeed: "5",
+        humidity: 1.2
+    }); // Render the index without passing any additional data
+});
+
 app.get("/getData", (req, res) => {
     const city = req.query.citty;
     const url = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${token}`;
@@ -23,7 +32,7 @@ app.get("/getData", (req, res) => {
             windSpeed: Math.round(data.wind.speed),
             humidity: Math.round(data.main.humidity)
 
-        });
+        }); // Render the index with krishna value
     }
 });
 
